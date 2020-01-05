7b0a0751d8ffc04a15366c9e16fa023d27379ab0
kg79 <klgalvan79@gmail.com>
2020-01-05 17:56:59 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const IP = '192.168.1.9';
+const express = require('express');
+const app = express();
+const path = require('path');
+const ejs = require('ejs');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+const serveStatic = require('serve-static');
+app.use(serveStatic(path.join(__dirname, 'public')));
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/favicon.ico', (req, res) => {
+    res.end('favicon')
+})
+
+if (module === require.main) {
+  const server = app.listen(process.env.PORT || 8080, IP, () => {
+    const port = server.address().port;
+    console.log(`${IP}:${port}`);
+  });
+
+}
+
+module.exports = app;
+
+
